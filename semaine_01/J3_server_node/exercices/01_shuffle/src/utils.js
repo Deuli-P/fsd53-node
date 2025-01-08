16030176d930e7945d3d789dd1b461dbf1231df1
bocarformation <bocsco3@gmail.com>
2025-01-08 12:45:15 +0100

    adding, Fisher Yates sort algo for exercice 1 day 3

--- semaine_01/J3_server_node/exercices/01_shuffle/src/utils.js
+++ semaine_01/J3_server_node/exercices/01_shuffle/src/utils.js
@@ -4,9 +4,15 @@ let html = `<ul>`
 
 // Tri par référence
 // -0,5 et 0,5
+//à ne pas utiliser lors de cas complexe de tri, mais très bien pour des tableaux de très petite longueur
 users.sort((a, b) => Math.random() - 0.5)
 
 // Tri de Fisher-Yates
+// Plus robuste et "parfait"
+    // for (let i = users.length - 1; i > 0; i--) {
+    //     const j = Math.floor(Math.random() * (i + 1));
+    //     [users[i], users[j]] = [array[j], array[i]];
+    // }
 
 
 // Boucle pour afficher sur la page HTML les utilisateurs modifiés
