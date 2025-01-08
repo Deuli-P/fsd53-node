8347111ebf0358b94c6c3b49ceaed1c53b0b192e
bocarformation <bocsco3@gmail.com>
2025-01-08 12:53:18 +0100

    add link for sort algo

--- semaine_01/J3_server_node/exercices/01_shuffle/src/utils.js
+++ semaine_01/J3_server_node/exercices/01_shuffle/src/utils.js
@@ -8,6 +8,7 @@ let html = `<ul>`
 users.sort((a, b) => Math.random() - 0.5)
 
 // Tri de Fisher-Yates
+// https://fr.wikipedia.org/wiki/M%C3%A9lange_de_Fisher-Yates#:~:text=Le%20m%C3%A9lange%20de%20Fisher%2DYates,m%C3%A9langer%20un%20ensemble%20d'objets.
 // Plus robuste et "parfait"
     // for (let i = users.length - 1; i > 0; i--) {
     //     const j = Math.floor(Math.random() * (i + 1));
